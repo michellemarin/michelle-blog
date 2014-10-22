b25e2753dc318bbd995ca9b6f9fa19ea3cae2250
Michelle Marin <michelle@carinocreative.com>
2014-10-21 23:03:34 -0400

    wja what happened to my brain?

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,41 @@
-console.log('The Iron Yard Rocks');
+var blogData = JSON.parse(localStorage.getItem('michelle-blog')) || {};
+var apiUrl   = 'http://tiny-pizza-server.herokuapp.com/collection/michelle-blog'
+
+var blog = function (postData) {
+
+    return {
+      template: _.template($('#post').html()),
+
+      data: _.extend(defualts, postData),
+
+      html: function() {return this.template(this.data);
+      },
+
+      save: function (doneCallback) {
+
+          $.ajax({
+            method: "POST",
+            url: apiUrl,
+            data: this.data
+          }).done(doneCallback);
+      }
+    };
+}
+
+var addPosts = function () {
+  var compiledTemplates = _.map(_.values(blogData), function (postData) {
+  var blog = new Blog(postData);
+  return blog.html();
+});
+  $('.container').html(compiledTemplates);
+
+  };
+
+setInterval(function () {
+  addPosts();
+    }, 1000);
+
+
+$('.submit-field input[type=submit]').on('click', onSend);
+
+})($);
